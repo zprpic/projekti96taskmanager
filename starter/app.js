fc0c5424991d62c8df6362ca2a0eccc902faaf2f
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-04 17:05:23 +0200

    connected to DB. using dotenv

--- starter/app.js
+++ starter/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
@@ -15,6 +16,15 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, (req, res) => {
-  console.log(`Server listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, (req, res) => {
+      console.log(`Server listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,2 +1,19 @@
-const connectionString =
-  "mongodb+srv://zprpic:<password>@nodeexpressprojects.berpc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
+
+/* .then(() => {
+    console.log("CONNECTED TO THE DB...");
+  })
+  .catch((err) => {
+    console.log(err);
+  }); */

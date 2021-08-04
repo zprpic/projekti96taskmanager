de39e42e1fc44e615dcb885f9b95575c77195932
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-04 18:47:30 +0200

    basic validation added for POST method

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,11 +1,14 @@
 //DB CONNECTION?
 
+const Task = require("../models/Task");
+
 const getAllTasks = (req, res) => {
   res.send("get all tasks");
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
 };
 
 const getTask = (req, res) => {

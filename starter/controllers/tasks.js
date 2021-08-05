35162bfca501417893cffb9277460c3fd310a09f
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-05 10:59:18 +0200

    GET all tasks api added

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -2,13 +2,22 @@
 
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-  res.send("get all tasks");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 const getTask = (req, res) => {

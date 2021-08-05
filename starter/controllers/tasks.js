469bb4134ab0add60b4263ee5cda19d64b55694b
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-05 11:32:40 +0200

    DELETE task api added

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -20,16 +20,31 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ message: `No task with ID: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 const updateTask = (req, res) => {
   res.send("task updated");
 };
 
-const deleteTask = (req, res) => {
-  res.send("task deleted");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.deleteOne({ _id: taskID });
+    res.status(200).json({ message: `Task with ID: ${taskID} deleted...` });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };

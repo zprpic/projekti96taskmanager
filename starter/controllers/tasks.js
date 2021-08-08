c2ca582a399bf0e102be0bffb54c8490c07e5f3a
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-08 10:45:23 +0200

    Custom error class added...

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,12 +1,13 @@
 const Task = require("../models/Task");
 const asyncWrapper = require("../middleware/async");
+const { createCustomError } = require("../errors/customError");
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
   res.status(200).json({ tasks });
 });
 
-const createTask = asyncWrapper(async (req, res) => {
+const createTask = asyncWrapper(async (req, res, next) => {
   const task = await Task.create(req.body);
   res.status(201).json({ task });
 });
@@ -15,7 +16,7 @@ const getTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ message: `No task with ID: ${taskID}` });
+    return next(createCustomError(`No task with ID: ${taskID}`, 404));
   }
 });
 
@@ -23,7 +24,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ message: `No task with ID: ${taskID}` });
+    return next(createCustomError(`No task with ID: ${taskID}`, 404));
   }
   res.status(200).json({ message: `${task} deleted...` });
 });
@@ -36,7 +37,7 @@ const updateTask = asyncWrapper(async (req, res) => {
   });
 
   if (!task) {
-    return res.status(404).json({ message: `No task with ID: ${taskID}` });
+    return next(createCustomError(`No task with ID: ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
--- starter/middleware/errorHandler.js
+++ starter/middleware/errorHandler.js
@@ -1,5 +1,12 @@
+const { CustomAPIError } = require("../errors/customError");
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-  return res.status(500).json({ message: err });
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+  return res
+    .status(500)
+    .json({ message: "Something went wrong. Please try again." });
 };
 
 module.exports = errorHandlerMiddleware;
